9cadb745825cfbb258f4088754c5c606a9a6f73d
Parth Panara <parthpanara9@gmail.com>
2022-03-20 00:06:06 -0400

    updated project-1 Menubar, updated git page, updated home page, added pages for PROJECT-2

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,52 +1,55 @@
-"""This test the homepage"""
+"""This test the GitPage"""
 
 def test_request_main_menu_links(client):
-    """This makes the index page"""
-    response = client.get("/")
+    """This checks the menubar on GitPage"""
+    response = client.get("/git")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<li class="nav-item"><a href="/" class="nav-link" aria-current="page">Home</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link active" href="/git">Git</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/docker">Docker</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/python">Python/Flask</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/cicd">CI/CD</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/terms">Terms</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/aaa">AAA</a></li>'in response.data
+    assert b' <li class="nav-item"><a class="nav-link" href="/oops">OOPS</a></li>'in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/solid">SOLID</a></li>' in response.data
+
+def test_request_git(client):
+    """This makes the index page"""
+    response = client.get("/git")
+    assert response.status_code == 200
+    assert b"Git" in response.data
+
+"""This test the HomePage"""
+
+
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Home Page" in response.data
+    assert b"Home" in response.data
 
-def test_request_about(client):
+def test_request_docker(client):
     """This makes the index page"""
-    response = client.get("/about")
+    response = client.get("/docker")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"Docker" in response.data
 
-def test_request_page1(client):
+def test_request_python(client):
     """This makes the index page"""
-    response = client.get("/page1")
+    response = client.get("/python")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"Python/Flask" in response.data
 
-def test_request_page2(client):
+def test_request_cicd(client):
     """This makes the index page"""
-    response = client.get("/page2")
+    response = client.get("/cicd")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
-
-def test_request_page3(client):
-    """This makes the index page"""
-    response = client.get("/page3")
-    assert response.status_code == 200
-    assert b"Page 3" in response.data
-
-def test_request_page4(client):
-    """This makes the index page"""
-    response = client.get("/page4")
-    assert response.status_code == 200
-    assert b"Page 4" in response.data
+    assert b"CI/CD" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
     response = client.get("/page5")
     assert response.status_code == 404
+
